ba5a0a99bb67ee9e7b6dbce19493a00e7b25c8aa
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-08-01 10:06:40 -0700

    Creating URL GET paths

--- Personal Portfolio Webpage/index.js
+++ Personal Portfolio Webpage/index.js
@@ -10,6 +10,16 @@ app.use(parser.json());
 app.use(express.static('public'));
 app.use(morgan('common'));
 
+app.get('/resume', (req, res) => {
+  res.sendFile('public/resume.html', { root: __dirname});
+});
+app.get('/aboutme', (req, res) => {
+  res.sendFile('public/aboutme.html', { root: __dirname});
+});
+app.get('/contact', (req, res) => {
+  res.sendFile('public/contact.html', { root: __dirname});
+});
+
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080');
 });
